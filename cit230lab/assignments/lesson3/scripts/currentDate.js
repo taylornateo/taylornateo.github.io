c560f2e4c99772c6a58edefd8ef4fbdc8d4f913e
taylornateo <tay18066@byui.edu>
2020-09-26 20:13:37 -0700

    Update - Future

---
+++ cit230lab/assignments/lesson3/scripts/currentDate.js
@@ -0,0 +1,4 @@
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+console.log(currentYear);
+document.getElementById("currentYear").innerHTML = currentYear;
---
+++ cit230lab/assignments/lesson6/scripts/lastModified.js
@@ -0,0 +1,3 @@
+let currentdate = document.lastModified;
+
+document.getElementById("currentdate").textContent = currentDate;
