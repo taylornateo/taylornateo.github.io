5a4b9229a8a74c2dff2abc3175fc3f771e587965
Taylor <TAYLON2@AD.CCSD.NET>
2020-10-31 14:18:23 -0700

    v5

--- cit230lab/assignments/lesson7/js/windchill.js
+++ cit230lab/assignments/lesson7/js/windchill.js
@@ -4,7 +4,7 @@ let temp = 54;
 buildWC(speed, temp);
 
 function buildWC(speed, temp) {
-    let windChill = document.getElementById(windChill);
+    let windChill = document.getElementById('wind-chill');
 
     //Compute the Windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
@@ -12,6 +12,7 @@ function buildWC(speed, temp) {
 
     //Round the integer
     wc = Math.floor(wc);
+
     wc = (wc > temp) ? temp : wc;
 
     //Display the Chill
